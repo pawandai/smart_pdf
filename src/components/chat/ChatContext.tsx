ba5bd3b3ab618939cb3b16355a11cf5075efaac6
Pawan Awasthi <pawanawasthi481@gmail.com>
2024-02-16 00:20:37 +0545

    feat: realtime ai chat streaming implemented

--- src/components/chat/ChatContext.tsx
+++ src/components/chat/ChatContext.tsx
@@ -2,6 +2,7 @@ import { trpc } from '@/app/_trpc/client';
 import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query';
 import { useMutation } from '@tanstack/react-query';
 import { ReactNode, createContext, useRef, useState } from 'react';
+import { toast } from 'sonner';
 
 type StreamResponse = {
   addMessage: () => void;
@@ -97,6 +98,91 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
         previousMessages:
           previousMessage?.pages.flatMap((page) => page.messages) ?? [],
       };
+    },
+    onSuccess: async (stream) => {
+      setIsLoading(false);
+
+      if (!stream) {
+        return toast.error(
+          'There was a problem sending this message. Please refres this page and try again.'
+        );
+      }
+
+      const reader = stream.getReader();
+      const decoder = new TextDecoder();
+      let done = false;
+
+      // accumulated response
+      let accResponse = '';
+
+      while (!done) {
+        const { value, done: doneReading } = await reader.read();
+        done = doneReading;
+        const chunkValue = decoder.decode(value);
+
+        accResponse += chunkValue;
+
+        // append chunk to the actual message
+        utils.getFileMessages.setInfiniteData(
+          { fileId, limit: INFINITE_QUERY_LIMIT },
+          (old) => {
+            if (!old) return { pages: [], pageParams: [] };
+
+            let isAiResponseCreated = old.pages.some((page) =>
+              page.messages.some((message) => message.id === 'ai-response')
+            );
+
+            let updatedPages = old.pages.map((page) => {
+              if (page === old.pages[0]) {
+                let updatedMessages;
+
+                if (!isAiResponseCreated) {
+                  updatedMessages = [
+                    {
+                      createdAt: new Date().toISOString(),
+                      id: 'ai-response',
+                      text: accResponse,
+                      isUserMessage: false,
+                    },
+                    ...page.messages,
+                  ];
+                } else {
+                  updatedMessages = page.messages.map((message) => {
+                    if (message.id === 'ai-response') {
+                      return {
+                        ...message,
+                        text: accResponse,
+                      };
+                    }
+
+                    return message;
+                  });
+                }
+                return {
+                  ...page,
+                  messages: updatedMessages,
+                };
+              }
+
+              return page;
+            });
+
+            return { ...old, pages: updatedPages };
+          }
+        );
+      }
+    },
+    onError: (_, __, context) => {
+      setMessage(backupMessage.current);
+      utils.getFileMessages.setData(
+        { fileId },
+        { messages: context?.previousMessages ?? [] }
+      );
+    },
+    onSettled: async () => {
+      setIsLoading(false);
+
+      await utils.getFileMessages.invalidate({ fileId });
     },
   });
 
