bf0742c675e3097157b1b946f6a3c8a131478573
Pawan Awasthi <pawanawasthi481@gmail.com>
2024-02-16 00:00:21 +0545

    feat: optimistic updates

--- src/components/chat/ChatContext.tsx
+++ src/components/chat/ChatContext.tsx
@@ -1,5 +1,7 @@
+import { trpc } from '@/app/_trpc/client';
+import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query';
 import { useMutation } from '@tanstack/react-query';
-import { ReactNode, createContext, useState } from 'react';
+import { ReactNode, createContext, useRef, useState } from 'react';
 
 type StreamResponse = {
   addMessage: () => void;
@@ -25,6 +27,10 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
   const [message, setMessage] = useState<string>('');
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
+  const utils = trpc.useContext();
+
+  const backupMessage = useRef('');
+
   const { mutate: sendMessage } = useMutation({
     mutationFn: async ({ message }: { message: string }) => {
       const response = await fetch('/api/message', {
@@ -38,6 +44,59 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
       if (!response.ok) throw new Error('Failed to send message');
 
       return response.body;
+    },
+
+    onMutate: async ({ message }) => {
+      backupMessage.current = message;
+      setMessage('');
+
+      // step 1:
+      await utils.getFileMessages.cancel();
+
+      // step 2:
+      const previousMessage = utils.getFileMessages.getInfiniteData();
+
+      // step 3:
+      utils.getFileMessages.setInfiniteData(
+        {
+          fileId,
+          limit: INFINITE_QUERY_LIMIT,
+        },
+        (old) => {
+          if (!old) {
+            return {
+              pages: [],
+              pageParams: [],
+            };
+          }
+
+          let newPages = [...old.pages];
+
+          let latestPage = newPages[0]!;
+
+          latestPage.messages = [
+            {
+              createdAt: new Date().toISOString(),
+              id: crypto.randomUUID(),
+              text: message,
+              isUserMessage: true,
+            },
+            ...latestPage.messages,
+          ];
+
+          newPages[0] = latestPage;
+          return {
+            ...old,
+            pages: newPages,
+          };
+        }
+      );
+      setIsLoading(true);
+
+      return {
+        previousMessages:
+          previousMessage?.pages.flatMap((page) => page.messages) ?? [],
+      };
     },
   });
 
