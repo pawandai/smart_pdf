4b7c5d6fa05cb378db1f7f004c56f7125c2cbe55
Pawan Awasthi <pawanawasthi481@gmail.com>
2024-02-16 15:18:10 +0545

    finishing touches

---
+++ src/components/shared/MobileNav.tsx
@@ -0,0 +1,95 @@
+import { ArrowRight, Menu } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+const MobileNav = ({ isAuth }: { isAuth: boolean }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleOpen = () => setIsOpen((prev) => !prev);
+
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (isOpen) toggleOpen();
+  }, [pathname]);
+
+  const closeOnCurrent = (href: string) => {
+    if (pathname === href) {
+      toggleOpen();
+    }
+  };
+
+  return (
+    <div className='sm:hidden'>
+      <Menu
+        onClick={toggleOpen}
+        className='relative z-50 h-5 w-5 text-zinc-700'
+      />
+
+      {isOpen ? (
+        <div className='fixed animate-in slide-in-from-top-5 fade-in-20 inset-0 z-0 w-full'>
+          <ul className='absolute bg-white border-b border-zinc-200 shadow-xl grid w-full px-10 pt-20 pb-8'>
+            {!isAuth ? (
+              <>
+                <li>
+                  <Link
+                    onClick={() => closeOnCurrent('/sign-up')}
+                    className='flex items-center w-full font-semibold text-green-600'
+                    href={'/sign-up'}
+                  >
+                    Get Started <ArrowRight className='ml-2 h-5 w-5' />
+                  </Link>
+                </li>
+                <li className='my-3 h-px w-full bg-gray-300' />
+                <li>
+                  <Link
+                    onClick={() => closeOnCurrent('/sign-in')}
+                    className='flex items-center w-full font-semibold text-green-600'
+                    href={'/sign-in'}
+                  >
+                    Sign In <ArrowRight className='ml-2 h-5 w-5' />
+                  </Link>
+                </li>
+                <li className='my-3 h-px w-full bg-gray-300' />
+                <li>
+                  <Link
+                    onClick={() => closeOnCurrent('/pricing')}
+                    className='flex items-center w-full font-semibold text-green-600'
+                    href={'/pricing'}
+                  >
+                    Pricing <ArrowRight className='ml-2 h-5 w-5' />
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link
+                    onClick={() => closeOnCurrent('/dashboard')}
+                    className='flex items-center w-full font-semibold text-green-600'
+                    href={'/dashboard'}
+                  >
+                    Dashboard <ArrowRight className='ml-2 h-5 w-5' />
+                  </Link>
+                </li>
+                <li className='my-3 h-px w-full bg-gray-300' />
+                <li>
+                  <Link
+                    className='flex items-center w-full font-semibold text-green-600'
+                    href={'/sign-out'}
+                  >
+                    Sign Out <ArrowRight className='ml-2 h-5 w-5' />
+                  </Link>
+                </li>
+                <li className='my-3 h-px w-full bg-gray-300' />
+              </>
+            )}
+          </ul>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default MobileNav;
--- src/components/shared/UploadButton.tsx
+++ src/components/shared/UploadButton.tsx
@@ -12,12 +12,14 @@ import { toast } from 'sonner';
 import { trpc } from '@/app/_trpc/client';
 import { useRouter } from 'next/navigation';
 
-const UploadDropzone = () => {
+const UploadDropzone = ({ isSubscribed }: { isSubscribed: boolean }) => {
   const [isUploading, setIsUploading] = useState<boolean>(false);
   const [uploadProgress, setUploadProgress] = useState<number>(0);
   const router = useRouter();
 
-  const { startUpload } = useUploadThing('pdfUploader');
+  const { startUpload } = useUploadThing(
+    isSubscribed ? 'proPlanUploader' : 'freePlanUploader'
+  );
 
   const { mutate: startPolling } = trpc.getFile.useMutation({
     onSuccess: (file) => {
@@ -87,7 +89,9 @@ const UploadDropzone = () => {
                   <span className='font-semibold'>Click to upload</span> or drag
                   n drop
                 </p>
-                <p className='text-xs text-zinc-500'>PDF (up to 4MB)</p>
+                <p className='text-xs text-zinc-500'>
+                  PDF (up to {isSubscribed ? '16' : '4'}MB)
+                </p>
               </div>
 
               {acceptedFiles && acceptedFiles[0] ? (
@@ -133,7 +137,7 @@ const UploadDropzone = () => {
   );
 };
 
-const UploadButton = () => {
+const UploadButton = ({ isSubscribed }: { isSubscribed: boolean }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
@@ -148,7 +152,7 @@ const UploadButton = () => {
       </DialogTrigger>
 
       <DialogContent>
-        <UploadDropzone />
+        <UploadDropzone isSubscribed={isSubscribed} />
       </DialogContent>
     </Dialog>
   );
